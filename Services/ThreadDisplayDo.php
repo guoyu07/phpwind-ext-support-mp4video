51e54d15e808e27bdd642912d8cee3f61cedd58a
Seven <shiweidu@outlook.com>
2017-03-02 15:11:43 +0800

    👏over.

---
+++ Services/ThreadDisplayDo.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Pw\Ext\SupportMP4Video\Services;
+
+use PwThreadDisplayDoBase;
+use PwThreadDisplay;
+
+class ThreadDisplayDo extends PwThreadDisplayDoBase
+{
+    protected $thread;
+
+    /**
+     * construct func.
+     *
+     * @param PwThreadDisplay $thread
+     * @author Seven Du <shiweidu@outlook.com>
+     * @homepage http://medz.cn
+     */
+    public function __construct(PwThreadDisplay $threadDisplay)
+    {
+        $this->thread = $threadDisplay->thread;
+    }
+
+    /**
+     * Build read,
+     *
+     * @param array $read thread info.
+     * @return array
+     * @author Seven Du <shiweidu@outlook.com>
+     * @homepage http://medz.cn
+     */
+    public function bulidRead($read)
+    {
+        $content = preg_replace_callback('/\<a\\s*href="([^\"]*?).mp4(.*?)".*?\>.*?\<\/a\>/is', function (array $matchs) {
+            $base = $matchs[1];
+            $query = $matchs[2];
+
+            $src = sprintf('%s.mp4%s', $base, $query);
+            $video = '
+                <video class="video-js" controls preload="auto" width="640" height="264" data-setup="{}">
+                    <source src="%s" type="video/mp4">
+                </video>
+            ';
+
+            return sprintf($video, $src);
+        }, $read['content']);
+
+        $read['content'] = $content;
+        return $read;
+    }
+
+    /**
+     * Run js.
+     *
+     * @author Seven Du <shiweidu@outlook.com>
+     * @homepage http://medz.cn
+     */
+    public function runJs()
+    {
+        include dirname(dirname(__FILE__)).'/resource/thread-footer.html';
+    }
+}
